8d3ad60fe7a1126ea883f07e227db455878011cf
Volkan Şah //NCF <VolkanSah@users.noreply.github.com>
2023-07-02 10:05:09 +0200

    Update gptcli.py

--- gptcli.py
+++ gptcli.py
@@ -11,7 +11,7 @@ def call_api(prompt):
     }
     headers = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer YOUR_OPENAI_API_KEY"
+        "Authorization": f"Bearer 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"
     }
     command = [
         "curl",
